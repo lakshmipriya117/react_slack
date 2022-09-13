813266b32ce0ce66fb27325c8fd25f1924aa8bd0
lakshmipriya <lakshmipriyar117@gmail.com>
2022-09-13 20:05:14 +0530

    final

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -22,30 +22,31 @@ const LoginForm = () => {
            </div>
              
 
-        <h1 className="heading">First,enter your email</h1>
-         <h2 className="s-head">We suggest using <b>the email address you use at work</b></h2>
+        <h1 className="heading">First of all,enter your email address</h1>
+         <h2 className="s-head">We suggest using the <b>email address that you use at work</b></h2>
 
 
             <input type="email" placeholder="name@work-email.com" required/>
              <div className="login-btn" onClick={popup}><b>Continue</b></div>
 
 
-            <h5>OR</h5>
+            <h5 className="or">OR</h5>
 
             {/* <a class="btn btn-secondary card__google" href="https://google.com" target=""_blank style={{color:"blue",background:"white",border:" solid blue",textAlign:"center"}}><i class="fab fa-google">
             </i>&nbsp;&nbsp;Continue with Google</a> */}
 
-            <a class="btn btn-lg " href="https://google.com"  style={{color:"#4285f4",background:"white",border:" solid #4285f4"}}><i class="fab fa-google"></i><b>&nbsp;&nbsp;Continue with Google</b></a>
-            <a class="btn btn-outline-light btn-lg " href="https://appleid.apple.com"  style={{color:"black",background:"white",border:" solid black"}}><i class="fab fa-apple"></i><b>&nbsp;&nbsp;Continue with Apple</b></a>
+
+            <a class=" btn-lg" href="https://google.com"  style={{color:"#4285f4",background:"white",border:"2px solid #4285f4",fontSize:"17px"}}><i class="fab fa-google"></i><b>&nbsp;&nbsp;Continue with Google</b></a>
+            <a class="btn-lg" href="https://appleid.apple.com"  style={{color:"black",background:"white",fontSize:"17px",border:" 2px solid black"}}><i class="fab fa-apple"></i><b>&nbsp;&nbsp;&nbsp;Continue with Apple</b></a>
             
 
 
             <p className="x">Already using slack?</p>
-            <a href="login.html" className="y">Sign into existing workplace</a>
+            <a href="login.html" className="y">Sign in to an existing workplace</a>
             <div className='link'>
-             <a href="#" style={{color:"black",padding:"20px 20px 20px 20px"}}>Privacy</a>
-             <a href="#" style={{color:"black",padding:"20px 20px 20px 20px"}} >Contact US</a>
-             <a href="#" style={{color:"black",padding:"20px 20px 20px 20px"}}>Change region</a>
+             <a href="#" style={{color:"#454245",padding:"10px 9px 32px 9px",fontSize:"15px"}}>Privacy & Terms</a>
+             <a href="#" style={{color:"#454245",padding:"10px 9px 32px 9px",fontSize:"15px"}} >Contact US</a>
+             <a href="#" style={{color:"#454245",padding:"10px 9px 32px 9px",fontSize:"15px"}}>Change region</a>
 
             </div>
 
